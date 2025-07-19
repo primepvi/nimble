ad16c6a6ac701e0a29f441b3ca3103f134049cac
primepvi <pedrobernardesv@gmail.com>
2025-07-19 20:24:54 -0300

    feat: creating create application use case.

---
+++ src/use-cases/create-application.ts
@@ -0,0 +1,41 @@
+import type { Application, ApplicationImage } from '@prisma/client';
+import { GenericError } from '@/errors/generic-error';
+import type { ApplicationsRepository } from '@/repositories/applications-repository';
+import type { UsersRepository } from '@/repositories/users-repository';
+
+export interface CreateApplicationUseCaseRequest {
+  ownerId: string;
+  image: ApplicationImage;
+  slug: string;
+  repository: string;
+  ram: number;
+  cpu: number;
+}
+
+export interface CreateApplicationUseCaseResponse {
+  application: Application;
+}
+
+export class CreateApplicationUseCase {
+  public constructor(
+    private applicationsRepository: ApplicationsRepository,
+    private usersRepository: UsersRepository
+  ) {}
+
+  public async handle(
+    applicationData: CreateApplicationUseCaseRequest
+  ): Promise<CreateApplicationUseCaseResponse> {
+    const existsOwnerUser = await this.usersRepository.findById(
+      applicationData.ownerId
+    );
+
+    if (!existsOwnerUser) {
+      throw new GenericError(400, 'Owner user not found.');
+    }
+
+    const application =
+      await this.applicationsRepository.create(applicationData);
+
+    return { application };
+  }
+}
