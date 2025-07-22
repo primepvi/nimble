74cc8e2dfd6f9fc89b53138460e155a675d43976
primepvi <pedrobernardesv@gmail.com>
2025-07-22 19:56:28 -0300

    feat: fetch authenticated user applications route.

---
+++ apps/api/src/controllers/me/fetch-applications.ts
@@ -0,0 +1,24 @@
+import { makeFetchUserApplicationsUseCase } from '@core/use-cases';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import z from 'zod';
+import { PrismaService } from '@/services';
+
+export const fetchApplicationsQuerySchema = z.object({
+  page: z.coerce.number().min(1).default(1),
+});
+
+export class FetchApplicationsController {
+  public async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { page } = fetchApplicationsQuerySchema.parse(request.query);
+    const userId = request.user.sub;
+
+    const fetchUserApplicationsUseCase =
+      makeFetchUserApplicationsUseCase(PrismaService);
+    const { applications } = await fetchUserApplicationsUseCase.handle({
+      userId,
+      page,
+    });
+
+    return reply.status(200).send({ applications });
+  }
+}
--- apps/api/src/controllers/me/index.ts
+++ apps/api/src/controllers/me/index.ts
@@ -4,6 +4,7 @@ import { GithubAuthenticator } from '@/services';
 import { bindHandler } from '@/utils/bind-handler';
 import { CreateApplicationController } from './create-application';
 import { CreateDeployController } from './create-deploy';
+import { FetchApplicationsController } from './fetch-applications';
 import { GetApplicationController } from './get-application';
 import { MeProfileController } from './profile';
 
@@ -19,8 +20,10 @@ export async function MeRoutes(app: FastifyInstance) {
   );
   const getApplicationController = new GetApplicationController();
   const createDeployController = new CreateDeployController();
+  const fetchApplicationsController = new FetchApplicationsController();
 
   app.post('/me/apps', bindHandler(createApplicationController));
+  app.get('/me/apps', bindHandler(fetchApplicationsController));
   app.get('/me/apps/:slug', bindHandler(getApplicationController));
   app.post('/me/apps/:slug/deploy', bindHandler(createDeployController));
 }
